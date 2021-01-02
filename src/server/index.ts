535da9cb227c2d4ca19b0a8fa5f510c94c1e4799
Seung Ho <shjang1992@gmail.com>
2021-01-03 03:30:54 +0900

    express-generator-typescript boilerplate created

--- src/server/index.ts
+++
@@ -1,6 +0,0 @@
-import { DnfCalculatorAddon } from "../server_addon/lib";
-
-const addon = new DnfCalculatorAddon("skyser2003");
-addon.greet("stranger");
-const weaponJson = addon.getWeaponJson();
-console.log(weaponJson);
